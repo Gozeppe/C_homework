39c41ca6b7157f6fdee84b8647aa715d8d796b15
belar <bel.arth92@gmail.com>
2022-12-08 12:57:18 +0300

    Homework 3

--- Third seminar/Задача 19/Задача 19.cs
+++ Third seminar/Задача 19/Задача 19.cs
@@ -6,21 +6,3 @@
 23432 -> да
 */
 
-Console.WriteLine("Введите число: ");
-string sum = Console.ReadLine();
-int len = sum.Length;
-if (len == 5)
-{
-    if (sum[0] == sum[4] && sum[1] == sum[3])
-    {
-        Console.WriteLine($"{sum} - Палиндром");
-    }
-    else
-    {
-        Console.WriteLine($"{sum} - НЕ палиндром");
-    }
-}
-else
-{
-    Console.WriteLine($"ОШИБКА: {sum} - не является пятизначным");
-}    
