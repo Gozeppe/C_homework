62926b34b1486ebcf390161aeaccdbf7f2f9bcf5
belar <bel.arth92@gmail.com>
2022-12-08 13:49:34 +0300

    Unfinished homework

---
+++ Seminar 3/Задача 21/Задача 21.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.WriteLine("Введите координаты точки A по оси X: ");
+int ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А по оси Y: ");
+int ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А по оси Z: ");
+int az = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите координаты точки B по оси X: ");
+int bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B по оси Y: ");
+int by = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B по оси Z: ");
+int bz = Convert.ToInt32(Console.ReadLine());
+
