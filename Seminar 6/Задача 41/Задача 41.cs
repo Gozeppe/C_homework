a6076413d0eaebb6ae5945f2a6d8a11178c90940
belar <bel.arth92@gmail.com>
2022-12-18 22:37:08 +0300

    6

--- Seminar 6/Задача 41/Задача 41.cs
+++ Seminar 6/Задача 41/Задача 41.cs
@@ -5,5 +5,34 @@
 1, -7, 567, 89, 223-> 3
 */
 
+Console.WriteLine("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] ManyNumbers = new int[m];
 
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    ManyNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int MoreThanZero(int[] ManyNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < ManyNumbers.Length; i++)
+  {
+    if(ManyNumbers[i] > 0 ) 
+    {
+    count += 1; 
+    }
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {MoreThanZero(ManyNumbers)} ");
 
